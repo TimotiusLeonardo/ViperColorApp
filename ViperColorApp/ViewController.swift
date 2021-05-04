3919c2dc5cd24a14a405a3b2fc0460e3f411d21e
Timotius Leonardo Lianoto <lionato14@gmail.com>
2021-05-04 16:47:46 +0700

    Initial Commit

---
+++ ViperColorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViperColorApp
+//
+//  Created by IDN MEDIA on 04/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
